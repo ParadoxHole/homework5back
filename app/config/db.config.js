b6296eb5e3a4708c86c4aaed6db0b3f6d0c917c4
ParadoxHole <paradoxholestudio@gmail.com>
2023-11-19 23:28:56 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "",
+    // DB: "myapi"
+    HOST: "sql",
+    USER: "root",
+    PASSWRD: "tsq9a29tn95gim3ar91c",
+    DB: "rpsonline"
+};
